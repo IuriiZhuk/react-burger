99c43307bd96f0ef87499253c402a1de1078ec3f
IuriiZhuk <zhukyuriu@gmail.com>
2018-08-21 16:09:55 +0300

    Add hamburger icon, and toggled Side Drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = props => (
-  <Auxiliary>
-    <Toolbar />
-    <main className={classes.Content}>{props.children}</main>
-  </Auxiliary>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
+
+  sideDrowerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false,
+    });
+  };
+  sideDrowerToggleHandler = () => {
+    this.setState(prevState => {
+      return {
+        showSideDrawer: !prevState.showSideDrawer,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <Auxiliary>
+        <Toolbar drawerToggleClicked={this.sideDrowerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrowerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Auxiliary>
+    );
+  }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,31 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
+import classes from './SideDrawer.css';
 
 const SideDrawer = props => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div>
-      <Logo />
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Auxiliary>
+      <Backdrop show={props.open} clicked={props.closed} />
+
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Auxiliary>
   );
 };
 
